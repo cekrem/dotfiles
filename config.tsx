da8c794e3ec739be4c1145b3ced48bcf0db25a5d
Christian Ekrem <cekrem@nepgroup.com>
2018-10-23 10:05:22 +0200

    make oni more barebones

--- config.tsx
+++ config.tsx
@@ -25,7 +25,21 @@ export const deactivate = (oni: Oni.Plugin.Api) => {
 export const configuration = {
     //add custom config here, such as
 
-    "ui.colorscheme": "onedark",
+    "oni.hideMenu"             : "hidden", // Hide top bar menu
+    "oni.loadInitVim"          : true, // Load user's init.vim
+    "oni.useDefaultConfig"     : false, // Do not load Oni's init.vim
+    "ui.colorscheme"           : "onedark", // Load init.vim colorscheme, remove this line if wants Oni's default
+    "autoClosingPairs.enabled" : false, // disable autoclosing pairs
+    "commandline.mode"         : false, // Do not override commandline UI
+    "wildmenu.mode"            : false, // Do not override wildmenu UI,
+    "tabs.mode"                : "native", // Use vim's tabline, need completely quit Oni and restart a few times
+//    "statusbar.enabled"        : false, // use vim's default statusline
+    "sidebar.enabled"          : false, // sidebar ui is gone
+//    "sidebar.default.open"     : false, // the side bar collapse 
+    "learning.enabled"         : false, // Turn off learning pane
+    "achievements.enabled"     : false, // Turn off achievements tracking / UX
+    "editor.typingPrediction"  : false, // Wait for vim's confirmed typed characters, avoid edge cases
+    "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
 
     //"oni.useDefaultConfig": true,
     //"oni.bookmarks": ["~/Documents"],
