1f0fa6e50347e340272b5eb1d97b19e7dd948b37
Christian Ekrem <cekrem@pm.me>
2021-04-05 12:09:21 +0200

    tweaks! :D

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,64 +1,25 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Christian Ekrem"
       user-mail-address "christianekrem@gmail.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-vibrant)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq frame-title-format "%b - cekrem Emacs")
 
 ;; Tweaks undeserving of their own file (for now)
+(setq display-line-numbers-type t)
 (setq initial-major-mode 'lisp-interaction-mode)
+
 (setq lsp-ui-doc-enable nil)
+(setq lsp-intelephense-multi-root nil)
+(setq lsp-solargraph-multi-root nil)
+(setq lsp-headerline-breadcrumb-enable 1)
+
 (add-to-list 'company-backends 'company-files)
 (ispell-change-dictionary "american" t)
 (hl-line-mode -1)
 (setq projectile-project-search-path '("~/code" "~/Dropbox"))
+
 (setq scroll-conservatively 0)
 (setq scroll-step 10)
-(setq frame-title-format "%b - cekrem Emacs")
 
 ;; Mac customization
 (when (equal system-type 'darwin)
