22db35274f69aa11b128c190b00862ac9df4dee2
Christian Ekrem <cekrem@pm.me>
2024-09-03 09:14:59 +0200

    tweak lazyvim and zprofile :)

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized" } },
     -- extra modules handled by LazyExtras (lazyvim.json)
     -- import/override with your plugins
     { import = "plugins" },
--- .zprofile
+++ .zprofile
@@ -2,5 +2,7 @@ source ~/.profile
 
 
 # Added by Toolbox App
-export PATH="$PATH:/Users/christian.ekrem/Library/Application Support/JetBrains/Toolbox/scripts"
+export PATH="$PATH:/Users/cekrem/Library/Application Support/JetBrains/Toolbox/scripts"
+
+# Make java work
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
