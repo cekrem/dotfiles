761056e770568bb1a8d19f241453ff2ccff49646
Christian Ekrem <christianekrem@gmail.com>
2025-06-27 13:36:53 +0200

    unbreak autocomplete with tab

--- .config/nvim/lua/plugins/index.lua
+++ .config/nvim/lua/plugins/index.lua
@@ -11,7 +11,20 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
-      keymap = { preset = "super-tab" },
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+      },
     },
   },
   {
